00d88f9e9285d64f8e8ef65653ee037b30879bc6
zemoso <shyamalika.naraboina@zemosolabs.com>
2024-09-18 21:23:14 +0530

    implemented up to molecules

---
+++ sonar-cloud/src/components/atoms/CustomTypography/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Typography } from "@mui/material";
+interface TypographyProps {
+  variant?: "h1" | "body1" | "body2";
+  Text: string;
+}
+const CustomTypography = ({ variant, Text }: TypographyProps) => {
+  return <Typography variant={variant}>{Text}</Typography>;
+};
+export default CustomTypography;
